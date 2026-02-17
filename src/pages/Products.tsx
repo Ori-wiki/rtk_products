ccf6a86c6a7cc9c64f31854a0b8c913983306696
Denis <Ori21wiki@gmail.com>
2026-02-18 02:54:30 +0300

    ant design

--- src/pages/Products.tsx
+++ src/pages/Products.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import {
   MenuFoldOutlined,
   MenuUnfoldOutlined,
@@ -6,7 +6,26 @@ import {
   UserOutlined,
   VideoCameraOutlined,
 } from '@ant-design/icons';
-import { Button, Layout, Menu, theme } from 'antd';
+import {
+  Button,
+  Layout,
+  Menu,
+  theme,
+  Card,
+  Col,
+  Row,
+  Flex,
+  Typography,
+} from 'antd';
+const { Meta } = Card;
+
+interface Product {
+  id: number;
+  title: string;
+  description: string;
+  thumbnail: string;
+  price: number;
+}
 
 const { Header, Sider, Content } = Layout;
 
@@ -16,8 +35,26 @@ const Products: React.FC = () => {
     token: { colorBgContainer, borderRadiusLG },
   } = theme.useToken();
 
+  const [products, setProducts] = useState<Product[]>([]);
+
+  console.log(products);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const res = await fetch('https://dummyjson.com/products');
+        const data = await res.json();
+        setProducts(data.products);
+      } catch (error) {
+        console.error('Ошибка при загрузке продуктов:', error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
+
   return (
-    <Layout style={{ width: '100%', height: '100vh' }}>
+    <Layout style={{ width: '100%', minHeight: '100vh', height: '100%' }}>
       <Sider trigger={null} collapsible collapsed={collapsed}>
         <div className='demo-logo-vertical' />
         <Menu
@@ -65,7 +102,47 @@ const Products: React.FC = () => {
             borderRadius: borderRadiusLG,
           }}
         >
-          Content
+          <Row gutter={[16, 16]}>
+            {products.map((product) => (
+              <Col span={4} key={product.id}>
+                <Card
+                  hoverable
+                  style={{
+                    width: '100%',
+                    height: '100%',
+                  }}
+                  cover={
+                    <img
+                      draggable={false}
+                      alt={product.title}
+                      src={product.thumbnail}
+                    />
+                  }
+                >
+                  <Meta
+                    title={product.title}
+                    description={
+                      <Flex
+                        vertical
+                        gap={8}
+                        justify='space-between'
+                        style={{ minHeight: '120px' }}
+                      >
+                        <Typography.Text>
+                          {product.description.length > 100
+                            ? product.description.slice(0, 100) + '...'
+                            : product.description}
+                        </Typography.Text>
+                        <Typography.Text
+                          style={{ fontWeight: 'bold' }}
+                        >{`$${product.price}`}</Typography.Text>
+                      </Flex>
+                    }
+                  />
+                </Card>
+              </Col>
+            ))}
+          </Row>
         </Content>
       </Layout>
     </Layout>
