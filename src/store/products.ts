cd0d82bde17e03ad549bc6076ad1fde3f9204c4f
Denis <Ori21wiki@gmail.com>
2026-02-18 03:52:08 +0300

    isOutDated logic

--- src/store/products.ts
+++ src/store/products.ts
@@ -48,19 +48,27 @@ export const productsSlice = createSlice({
 const { reducer: productsReducer, actions } = productsSlice;
 const { productsRequested, productsReceived, productsRequestFailed } = actions;
 
-export const loadProductsList = () => async (dispatch: Dispatch) => {
-  dispatch(productsRequested());
-  try {
-    const res = await fetch('https://dummyjson.com/products');
-    if (!res.ok) {
-      throw new Error('Failed to fetch products');
+export const loadProductsList =
+  () => async (dispatch: Dispatch, getState: () => RootState) => {
+    const { lastFetch } = getState().products;
+    const isOutDated = lastFetch
+      ? (new Date().getTime() - new Date(lastFetch).getTime()) / 1000 / 60 > 10
+      : true;
+
+    if (isOutDated) {
+      dispatch(productsRequested());
+      try {
+        const res = await fetch('https://dummyjson.com/products');
+        if (!res.ok) {
+          throw new Error('Failed to fetch products');
+        }
+        const data = await res.json();
+        dispatch(productsReceived(data.products));
+      } catch (error) {
+        dispatch(productsRequestFailed((error as Error).message));
+      }
     }
-    const data = await res.json();
-    dispatch(productsReceived(data.products));
-  } catch (error) {
-    dispatch(productsRequestFailed((error as Error).message));
-  }
-};
+  };
 
 export const getProducts = (state: RootState) => state.products.entitties;
 export const getProductsLoadingStatus = (state: RootState) =>
